835f853ecc5ee2cb436574686f5dccec87bc5e37
JonArbell <138678861+JonArbell@users.noreply.github.com>
2024-08-16 20:52:53 +0800

    add docker file and add another comment in main class

--- src/main/java/com/myapp/pea/PeaToDoListApplication.java
+++ src/main/java/com/myapp/pea/PeaToDoListApplication.java
@@ -16,7 +16,8 @@ public class PeaToDoListApplication {
 * Artifact 		: 	PeaTodoListApplication
 * Name			: 	PEA TO-DO LIST APPLICATION
 * Description	:	My First Web Application
+* JDK version	:	21.0.1
 * Package name  :	com.myapp.pea
-* 
+*
 */
 }
