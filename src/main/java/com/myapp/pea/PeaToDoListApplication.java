98ae824c4aaed4ce8e7248bf1bd285363e313841
JonArbell <138678861+JonArbell@users.noreply.github.com>
2024-08-16 20:37:46 +0800

    add some codes in main java class then remove it

--- src/main/java/com/myapp/pea/PeaToDoListApplication.java
+++ src/main/java/com/myapp/pea/PeaToDoListApplication.java
@@ -3,8 +3,6 @@ package com.myapp.pea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class PeaToDoListApplication {
 
