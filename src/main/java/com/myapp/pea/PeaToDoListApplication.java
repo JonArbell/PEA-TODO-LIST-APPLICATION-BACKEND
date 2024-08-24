4895a691b1ac1cfa4d7841db141ab640ed6f6a0b
JonArbell <138678861+JonArbell@users.noreply.github.com>
2024-08-24 11:26:37 +0800

    remove practice code in main class

--- src/main/java/com/myapp/pea/PeaToDoListApplication.java
+++ src/main/java/com/myapp/pea/PeaToDoListApplication.java
@@ -2,7 +2,6 @@ package com.myapp.pea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class PeaToDoListApplication {
@@ -10,26 +9,6 @@ public class PeaToDoListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PeaToDoListApplication.class, args);
 
-		final var base = 3;
-		int[] x = new int[base];
-		var s = new Scanner(System.in);
-
-		for(var i = 0; i < base; i++){
-			var num = s.nextInt();
-			System.out.println("Input "+ (i+1) + " : "+num);
-			x[i] = num;
-		}
-
-		var temp = x[0];
-		for(var i = 1; i < base; i++){
-
-			if(temp < x[i]){
-				temp = x[i];
-			}
-
-		}
-
-		System.out.println("Largest num is : "+temp);
 	}
 /*
 *
