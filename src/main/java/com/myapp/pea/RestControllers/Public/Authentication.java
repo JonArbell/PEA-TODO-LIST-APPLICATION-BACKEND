196ed3637f0ada77db4c365d9182409e2981f169
JonArbell <138678861+JonArbell@users.noreply.github.com>
2024-09-11 03:01:27 +0800

    remove unecessary lines in authentication.java and add this comment earlier because I already add create account functionality. Ui and request

--- src/main/java/com/myapp/pea/RestControllers/Public/Authentication.java
+++ src/main/java/com/myapp/pea/RestControllers/Public/Authentication.java
@@ -19,8 +19,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
