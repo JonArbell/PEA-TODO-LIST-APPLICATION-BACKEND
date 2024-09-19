b9fff5204b60866658c014d54526868721742134
JonArbell <138678861+JonArbell@users.noreply.github.com>
2024-09-19 17:43:22 +0800

    fix the no list view

--- src/main/java/com/myapp/pea/Views/Authenticated/ListsPage/ListsSortedByDateModified.java
+++ src/main/java/com/myapp/pea/Views/Authenticated/ListsPage/ListsSortedByDateModified.java
@@ -1,20 +1,34 @@
 package com.myapp.pea.Views.Authenticated.ListsPage;
 
 import com.myapp.pea.Services.AccountService.UserService;
+import com.myapp.pea.Services.ListsService.GetLists;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @AllArgsConstructor
 public class ListsSortedByDateModified {
 
     private final UserService userService;
+    private final GetLists getLists;
 
     @GetMapping("/list/{id}")
-    public String getList(){
+    public String getList(@PathVariable Long id){
 
         if(!userService.isUserAuthenticated()){
+            return "redirect:/";
+        }
+
+        var getList = getLists
+                .allListsDateModified()
+                .stream()
+                .filter(list -> list != null && list.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        if(getList == null){
             return "redirect:/";
         }
 
