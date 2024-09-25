2730c60b9d1ef11b9dc657cd8bb1d754dc5b9a0d
JonArbell <138678861+JonArbell@users.noreply.github.com>
2024-09-25 22:22:55 +0800

    I add initial design for todo container

--- src/main/resources/static/js/authenticated/uiInteraction/todoContainer.js
+++ src/main/resources/static/js/authenticated/uiInteraction/todoContainer.js
@@ -1,4 +1,4 @@
-export const createTodoContainer = async (data) => {
+export const createTodoContainer = async (todos) => {
     
     if(window.location.pathname !== '/about-us' && window.location.pathname !== '/contact-us'){
         const mainContainer = document.querySelector('#todos-container');
@@ -7,38 +7,47 @@ export const createTodoContainer = async (data) => {
         const totalTasks = document.querySelector('#total-tasks');
         totalTasks.innerHTML = '';
 
-        if(data.length === 0){
+        totalTasks.textContent = `${todos.length}`;
+        if(todos.length === 0){
             document.querySelector('#no-tasks-display').style.display = 'block';
-            totalTasks.textContent = `0`;
             return;
         }
         document.querySelector('#no-tasks-display').style.display = 'none';
 
-        data.forEach(todo => {
+        todos.forEach(todo => {
 
             const todoContainer = document.createElement('div');
-            todoContainer.classList.add('todo-container');
+            todoContainer.classList.add(
+                'border-2',
+                'border-solid',
+                'border-green-color',
+                'rounded-2xl',
+                'flex',
+                'flex-col',
+                'w-[25vw]',
+                'h-30vh'
+            );
             todoContainer.id = `${todo.id}`;
             todoContainer.innerHTML = `
-                            <h2 id="${todo.id}" onclick="ViewDetails.viewDetails(${todo.id})">View Details</h2>
+                            <h2 id="${todo.id}" class="text-center cursor-pointer text-green-color font-bold" onclick="ViewDetails.viewDetails(${todo.id})" role="button">View Details</h2>
             
-                            <div class="todo-container-header">
-                                <h3>Title</h3>
-                                <h3>Completed</h3>
-                                <h3>Target Date</h3>
+                            <div class="flex justify-center w-[90%]">
+                                <h3 class="h-[10vh]">Title</h3>
+                                <h3 class="h-[10vh]">Completed</h3>
+                                <h3 class="h-[10vh]">Target Date</h3>
+                            </div>
+        
+                            <hr class="bg-green-color h-2"/>
+            
+                            <div class="flex justify-center w-[90%]">
+                                <p class="h-[10vh]">${todo.title}</p>
+                                <p class="h-[10vh]">${todo.done? 'Yes' : 'No'}</p>
+                                <p class="h-[10vh]">${todo.formattedDate}</p>
                             </div>
         
                             <hr/>
             
-                            <div class="todo-container-data">
-                                <p class="title">${todo.title}</p>
-                                <p class="completed">${todo.done? 'Yes' : 'No'}</p>
-                                <p class="target-date">${todo.formattedDate}</p>
-                            </div>
-        
-                            <hr/>
-            
-                            <div>
+                            <div class="flex justify-center h-1/4">
                                 <button class="delete-todo-button" onclick="CrudTodoUi.deleteTodoUi(${todo.id})">Delete</button>
                                 <button class="mark-as-done-todo-button" onclick="CrudTodoUi.todoMarkAsDoneUi(${todo.id})">Mark as complete</button>
                                 <button class="edit-todo-button" onclick="CrudTodoUi.editTodoUi(${todo.id})">Edit to-do</button>
@@ -49,8 +58,6 @@ export const createTodoContainer = async (data) => {
             mainContainer.appendChild(todoContainer);
         });
     
-        totalTasks.textContent = `${data.length}`;
-        
     }
     
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
               "80vh": "80vh",
               "85vh": "85vh",
               "90vh": "90vh",
-              "95vh": "95vh",
+              "95vh": "95vh"
       },
       width : {
         "10vw" : "10vw",
@@ -29,11 +29,11 @@ module.exports = {
         "30vw" : "30vw",
         "40vw" : "40vw",
         "50vw" : "50vw",
+        "50%" : "50%",
         "60vw" : "60vw",
         "70vw" : "70vw",
         "80vw" : "80vw",
-        "90vw" : "90vw",
-        "97.5%"     : "97.5%"
+        "90vw" : "90vw"
       },
       backgroundColor : {
         "green-bg" : "#254336",
