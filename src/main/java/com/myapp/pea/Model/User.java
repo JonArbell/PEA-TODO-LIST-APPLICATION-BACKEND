b32df3be2631dfb9314a922b4855b96c2f328443
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-02-05 13:08:41 +0800

    add h2 database and spring data jpa dependency

---
+++ src/main/java/com/myapp/pea/Model/User.java
@@ -0,0 +1,23 @@
+package com.myapp.pea.Model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long googleId;
+    private String fullName;
+    private String email;
+
+}
